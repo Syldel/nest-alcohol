05fefba58d3a649640cd41add4e7405c306439c7
Syldel <nightfand@hotmail.com>
2025-02-19 12:12:59 +0100

    feat(database): post-save logs

--- src/alcohol/entities/alcohol.middleware.ts
+++ src/alcohol/entities/alcohol.middleware.ts
@@ -2,7 +2,8 @@ import * as fs from 'fs-extra';
 
 export async function backupDocument(doc) {
   try {
-    const backupFilePath = './jsons/mongo-live-backup.json';
+    const targetKeyword = 'whisky';
+    const backupFilePath = `./jsons/${targetKeyword}-mongo-backup.json`;
     const collectionName = this.collection.name;
 
     // Charger ou initialiser le fichier de backup
@@ -14,10 +15,22 @@ export async function backupDocument(doc) {
     backupData[collectionName] = backupData[collectionName] || [];
     backupData[collectionName].push(doc);
 
+    console.log(
+      backupData[collectionName].length,
+      `${targetKeyword}(s) in Database`,
+    );
+
     // Sauvegarder les données mises à jour
     await fs.writeJson(backupFilePath, backupData, { spaces: 2 });
-    console.log(`Backup mis à jour après l'insertion dans ${collectionName}`);
+    console.log(
+      '\x1b[32m' +
+        `Backup mis à jour après l'insertion dans ${collectionName}` +
+        '\x1b[0m',
+    );
   } catch (error) {
-    console.error('Erreur lors du backup live:', error);
+    console.error(
+      '\x1b[31m' + 'Erreur lors du backup live:' + '\x1b[0m',
+      error,
+    );
   }
 }
